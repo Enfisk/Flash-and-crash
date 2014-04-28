592c44b243eb02333d09d288bb9f8a356c26c5a1
Enfisk <wild_cat1994@hotmail.com>
2014-04-28 14:58:12 +0200

    Respawn.cs

--- FlashAndCrash/Assets/Scripts/Respawn.cs
+++ FlashAndCrash/Assets/Scripts/Respawn.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class Respawn : MonoBehaviour {
+    public GameObject lastSpawnPoint;
+    public Vector3 offset;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        lastSpawnPoint = GameObject.Find("Spawn Point");
+    }
+
+    public void Respawn()
+    {
+        if (lastSpawnPoint == null)
+        {
+            return;
+        }
+
+        transform.position = lastSpawnPoint.transform.position + offset;
+        //transform.position = new Vector3(lastSpawnPoint.transform.position.x, lastSpawnPoint.transform.position.y, lastSpawnPoint.transform.position.z);
+    }
 }
