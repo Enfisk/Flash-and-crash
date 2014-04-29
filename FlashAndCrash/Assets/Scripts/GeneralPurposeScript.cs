58a1cbab148135064d0517f31c1bf7832f5fa525
Enfisk <wild_cat1994@hotmail.com>
2014-04-29 15:15:47 +0200

    General Purpose Script
    Make the mouse "disappear", exit on esc and alt-f4

---
+++ FlashAndCrash/Assets/Scripts/GeneralPurposeScript.cs
@@ -0,0 +1,32 @@
+﻿//Remove cursor, allow for alt-f4/escape to quit
+
+using UnityEngine;
+using System.Collections;
+
+public class GeneralPurposeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.lockCursor = true;
+	}
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || ((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && Input.GetKeyDown(KeyCode.F4)))
+        {
+            if (Application.isEditor)
+            {
+                Screen.lockCursor = false;
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
