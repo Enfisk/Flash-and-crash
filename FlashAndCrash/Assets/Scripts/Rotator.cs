08dbfcf13e91f65785133f684b2c4084d753ec42
Swagathor <adam.wrange@gmail.com>
2014-05-15 17:21:24 +0200

    changes to level
    added more assets into the game and changed walls

---
+++ FlashAndCrash/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    public float Degrees = 40.0f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0 ,Degrees * Time.deltaTime, 0);
+
+	
+	}
+}
