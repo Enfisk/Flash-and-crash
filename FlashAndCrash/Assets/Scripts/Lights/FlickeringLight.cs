417d61cf0039435e08c0da32d7f1fb1f068ebd73
Enfisk <wild_cat1994@hotmail.com>
2014-05-15 14:55:50 +0200

    Flicker lights on player collision
    Also added a script to automatically flicker lights

--- FlashAndCrash/Assets/Scripts/Lights/FlickeringLight.cs
+++ FlashAndCrash/Assets/Scripts/Lights/FlickeringLight.cs
@@ -2,36 +2,33 @@
 using System.Collections;
 
 [RequireComponent(typeof(Light))]
-public class FlickeringLight : MonoBehaviour {
+public class FlickeringLight : MonoBehaviour
+{
     public float flickeringTime = 0.5f;     //The total time where the light flickers
     public float timeBetweenFlickers = 0.5f;
-    [Range(0, 8)] public float minIntensity = 0.0f;
-    [Range(0, 8)] public float maxIntensity = 1.0f;
+
+    [Range(0, 8)]
+    public float minIntensity = 0.0f;
+    [Range(0, 8)]
+    public float maxIntensity = 1.0f;
 
     private float originalIntensity;
-    private bool flickering = false;
-    private float timeSinceLastFlicker;
+    [HideInInspector] public bool flickering = false;
+    [HideInInspector] public float timeSinceLastFlicker;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         originalIntensity = light.intensity;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (!flickering)
-        {
-            if (timeSinceLastFlicker >= timeBetweenFlickers && Random.Range(0, 10) == 3)
-            {
-                flickering = true;
-                StartCoroutine(FlickerLights());
-            }
+    }
 
-            timeSinceLastFlicker += Time.deltaTime;
-        }
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastFlicker += Time.deltaTime;
+    }
 
-    IEnumerator FlickerLights()
+    public IEnumerator FlickerLights()
     {
         float timePassed = 0.0f;
 
