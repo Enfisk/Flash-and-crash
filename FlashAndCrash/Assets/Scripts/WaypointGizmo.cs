224dc6a98900682ada00607b109ef92e14519b6c
Enfisk <wild_cat1994@hotmail.com>
2014-05-19 11:43:20 +0200

    WaypointGizmo, new shader, image effects

---
+++ FlashAndCrash/Assets/Scripts/WaypointGizmo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class WaypointGizmo : MonoBehaviour
+{
+    private GameObject platform;
+
+    void Awake()
+    {
+        foreach (Transform child in transform.parent)
+        {
+            if (child.name.Contains("Platform") || child.name.Contains("Box"))
+            {
+                Debug.Log("Found it! " + child.name);
+                platform = child.gameObject;
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, platform.renderer.bounds.size);
+    }
+}
