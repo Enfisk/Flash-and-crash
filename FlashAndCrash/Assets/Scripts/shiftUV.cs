17cd4fff8e04f3cf6973d847f6f25065b9916c8a
Swagathor <adam.wrange@gmail.com>
2014-05-26 18:36:06 +0200

    added finish line

---
+++ FlashAndCrash/Assets/Scripts/shiftUV.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shiftUV : MonoBehaviour {
+    public Material material;
+
+    void OnTriggerEnter()
+    {
+        material.mainTextureOffset = new Vector2(0.5f, 0);
+    }
+}
