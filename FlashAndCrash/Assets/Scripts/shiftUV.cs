cba660bc3ff9d75ee9df9833ed9ec15e84f2d50a
Swagathor <adam.wrange@gmail.com>
2014-05-27 13:35:04 +0200

    Major tom
    added finish line one more time

--- FlashAndCrash/Assets/Scripts/shiftUV.cs
+++ FlashAndCrash/Assets/Scripts/shiftUV.cs
@@ -4,8 +4,24 @@ using System.Collections;
 public class shiftUV : MonoBehaviour {
     public Material material;
 
-    void OnTriggerEnter()
+    private Color baseColor;
+
+    void Awake()
     {
-        material.mainTextureOffset = new Vector2(0.5f, 0);
+        baseColor = material.color;
+    }
+
+    void OnTriggerEnter(Collider P_thing)
+    {
+        if (P_thing.tag.Contains("ball"))
+        {
+            material.color = new Color(0f, 1f, 0f);
+            material.mainTextureOffset = new Vector2(0.5f, 0);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        material.color = baseColor;
     }
 }
