53d6852ebc9cf69b1590ac2823854b58b9f38efa
Enfisk <wild_cat1994@hotmail.com>
2014-06-11 21:21:10 +0200

    Activate line

---
+++ FlashAndCrash/Assets/Scripts/Activators/ActivatorLine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivatorLine : BaseActivatee {
+    public Material copyMat;
+    public Vector2 offset;
+
+    private Material mat;
+
+	// Use this for initialization
+	void Start () {
+        mat = new Material(copyMat);
+        renderer.material = mat;
+	}
+
+    public override void Activate()
+    {
+        mat.mainTextureOffset = new Vector2(mat.mainTextureOffset.x + offset.x, mat.mainTextureOffset.y + offset.y);
+    }
+}
